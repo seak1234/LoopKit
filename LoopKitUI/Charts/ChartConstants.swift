3b7a32a9b3dd9d40c58eaf37525b7efda66bb058
David Schnitzler <davidschnitzler@Davids-MacBook-Air.local>
2026-09-21 11:07:54 -0400

    Expand default glucose chart range

--- LoopKitUI/Charts/ChartConstants.swift
+++ LoopKitUI/Charts/ChartConstants.swift
@@ -16,7 +16,7 @@ public enum ChartConstants {
     public static let statusChartMinimumHistoryDisplay: TimeInterval = .hours(1)
 
     public static let glucoseChartDefaultDisplayBound =
-        HKQuantity(unit: .milligramsPerDeciliter, doubleValue: 100)...HKQuantity(unit: .milligramsPerDeciliter, doubleValue: 175)
+        HKQuantity(unit: .milligramsPerDeciliter, doubleValue: 50)...HKQuantity(unit: .milligramsPerDeciliter, doubleValue: 200)
 
     public static let glucoseChartDefaultDisplayRangeWide =
         HKQuantity(unit: .milligramsPerDeciliter, doubleValue: 60)...HKQuantity(unit: .milligramsPerDeciliter, doubleValue: 200)
